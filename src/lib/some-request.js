599e2ad2078ded2470d739cd7be022e90f437e64
TonyJev93 <tonyJev93@gmail.com>
2023-04-18 02:27:28 +0900

    class validator 적용, 성공 확인

--- src/lib/some-request.js
+++ src/lib/some-request.js
@@ -1,3 +1,5 @@
+import {validate, Length} from "class-validator";
+
 const readOnly = (target, property, descriptor) => { // with new Person, target will be an instance of Person
     descriptor.value = () => {return 'hello, decorators!'}
     descriptor.writable = false;
@@ -6,10 +8,15 @@ const readOnly = (target, property, descriptor) => { // with new Person, target 
 
 
 export class Person {
+    @Length(0, 10)
+    title = "title"
+
     constructor(
+        title,
         firstName,
         lastName
     ) {
+        this.title = title
         this.firstName = firstName
         this.lastName = lastName
     }
@@ -17,6 +24,11 @@ export class Person {
     @readOnly
     getFullName() {
         return `${this.firstName} ${this.lastName}`
+    }
+
+    async validateForTest() {
+        const isValid = await validate(this)
+        console.log("isValid : ", isValid)
     }
 }
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
     import {Person} from "../lib/some-request.js";
 
-    export const person = new Person('계원', '전')
+    export const person = new Person('12345678901','계원', '전')
     console.log(person.getFullName())
+
+    person.validateForTest()
 </script>
 
-<h1>Decorator Test</h1>
+<h1>{person.title}</h1>
 <p>
     {person.firstName}<br/>
     {person.lastName}<br/>
