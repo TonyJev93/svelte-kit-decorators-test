4c63b06c086aac613208f5504c2c8f51d8cc369b
TonyJev93 <tonyJev93@gmail.com>
2023-04-18 01:08:31 +0900

    add vite-plugin-babel dependency

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import babel from 'vite-plugin-babel'
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		babel()
+	]
 });
